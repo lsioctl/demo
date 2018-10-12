df519b0babf8150484a279174e66db871921ebff
lsioctl <laurent.steff@gmail.com>
2018-10-12 13:59:06 +0800

    predemo test

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
+version = "3"
+
 @app.route("/")
 def hello():
-    return "Hello World v2" 
+    return "Hello World" + version 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)

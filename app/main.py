f37fa288832f1c9c89503906ae94432516da9750
lsioctl <laurent.steff@gmail.com>
2018-10-16 15:41:36 +0800

    improved swarm mode

--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-version = "3"
+version = "4"
 
 @app.route("/")
 def hello():
-    return "Hello World" + version 
+    return "Hello World version: " + version 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)

3ebdc11e041e719d96240c259b5f72be9f0aa119
lsioctl <laurent.steff@gmail.com>
2018-10-23 15:57:22 +0800

    test

--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-version = "9"
+version = "10"
 
 @app.route("/")
 def hello():
-    return "Hello World version: " + version 
+    return "Hello World version: " + version
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)
